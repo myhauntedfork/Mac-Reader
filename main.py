f717744eadb144207ffbfb9e168e86a754094d87
myhauntedspoon <drewhinckley2009@gmail.com>
2024-11-11 11:34:42 -0600

    add exception & check platform

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import subprocess
+import platform
 
 message = input("What would you like you MAC to say?\n>> ")
 def say_something(txt):
-        subprocess.run(["say", txt])
+        system = platform.system()
+        try:
+                if system == "Darwin":
+                        subprocess.run(["say", txt])
+        except Exception as e:
+                print(f"Error: {e}")
 say_something(message)
